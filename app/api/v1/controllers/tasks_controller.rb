45973b6a48f4752e0c978dae1909ccdb39f9aaa2
ArslanInfinite <ali.arslan319@gmail.com>
2021-05-22 21:25:47 -0400

    backend overhaul, use generators to create task model, controller, routes, serializers, establish cors

---
+++ app/api/v1/controllers/tasks_controller.rb
@@ -0,0 +1,34 @@
+class TasksController < ApplicationController
+
+  def index 
+    @tasks = Task.all
+    render json: @tasks
+  end
+
+  def show
+    @task = Task.find(params[:id])
+    render json: @task
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      render json: @task
+    else
+      render json: {error: 'Error creating task'}
+    end
+  end
+
+  # def update
+  #   @task = Task.find(params[:id])
+  #   @task.update(title: params["task"]["title"])
+  #   @task.save
+  #   render json: @task
+  # end
+
+   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
+end
